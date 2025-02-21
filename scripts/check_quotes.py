2598f5eb97b66f342dd8cfaa2d5aba064faadeba
Laurent FAVOLE <laurentfavole03@gmail.com>
2025-02-21 10:18:49 +0100

    CI: Check quotes format

--- scripts/check_quotes.py
+++ scripts/check_quotes.py
@@ -1,11 +1,17 @@
 """Check that the quotes are correctly formatted."""
 
+import sys
+import warnings
 from pathlib import Path
 
 from quotes import Folder
 
 if __name__ == "__main__":
-    for path in Path(__file__).parent.parent.glob("**/0.json"):
-        folder = Folder(path.parent)
-        print(f"Checking {folder.path}...")
-        folder.check()
+    with warnings.catch_warnings(record=True) as w:
+        for path in Path(__file__).parent.parent.glob("**/0.json"):
+            folder = Folder(path.parent)
+            print(f"Checking {folder.path}...")
+            folder.check()
+
+        if w:
+            sys.exit(1)
